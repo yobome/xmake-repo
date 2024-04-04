24525b1cb6637c82e1aeb24255a3575d5579d2e4
yobome <work.ymo@outlook.com>
2024-04-05 03:14:01 +0800

    add stlog v1.0.0

---
+++ packages/s/stlog/xmake.lua
@@ -0,0 +1,23 @@
+package("stlog")
+    set_description("The stlog package")
+    add_deps("fmt 10.1.1")
+
+    add_urls("git@github.com:yobome/stlog.git")
+    add_versions("v1.0.0", "ad2a56e8618146da1702d220f2e5fa447cfc5838")
+
+    if is_plat("linux") then
+        add_syslinks("pthread")
+    end
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
