80e03086279a7a3686d5b42f12db2d7186c49ac0
yobome <work.ymo@outlook.com>
2024-04-06 02:36:25 +0800

    update stlog to v1.0.1

--- packages/s/stlog/xmake.lua
+++ packages/s/stlog/xmake.lua
@@ -4,6 +4,7 @@ package("stlog")
 
     add_urls("git@github.com:yobome/stlog.git")
     add_versions("v1.0.0", "ad2a56e8618146da1702d220f2e5fa447cfc5838")
+    add_versions("v1.0.1", "1d32579a642af7654f3fe2a3473f7e0d07a055dc")
 
     if is_plat("linux") then
         add_syslinks("pthread")
